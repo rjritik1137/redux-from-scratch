d0e931bb56f8f405a80ca7dc2ce994c0954639ad
Ritik Jain <300073191@BLRM-300073191.local>
2022-10-13 23:05:41 +0530

    added prettier

--- src/index.js
+++ src/index.js
@@ -3,23 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { DECREMENT, INCREAMENT } from './constants'
-import { createStore } from './redux/store'
+import { DECREMENT, INCREAMENT } from './constants';
+import { createStore } from './redux/store';
 
-
-const reducer = (state = {count:0}, action)=>{
-  switch(action.type){
-    case INCREAMENT: return {...state, count: state.count +1  }
-    case DECREMENT: return {...state, count: state.count - 1 }
-    default: return state
-  }
-}
-export const store = createStore(reducer,  {count:0})
+const reducer = (state = { count: 0 }, action) => {
+    switch (action.type) {
+        case INCREAMENT:
+            return { ...state, count: state.count + 1 };
+        case DECREMENT:
+            return { ...state, count: state.count - 1 };
+        default:
+            return state;
+    }
+};
+export const store = createStore(reducer, { count: 0 });
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/redux/createStore.js
+++ src/redux/createStore.js
@@ -1,29 +1,27 @@
+const createStore = (reducer, preLoadedState) => {
+  let currentState = preLoadedState;
+  let currentListeners = [];
 
+  return {
+    getState() {
+      return currentState;
+    },
+    dispatch(action) {
+      currentState = reducer(currentState, action);
+      currentListeners.forEach((fn) => fn(currentState));
+      return action;
+    },
+    subscribe(fn) {
+      let isSubscribed = true;
+      currentListeners.push(fn);
+      return () => {
+        if (!isSubscribed) return;
+        const index = currentListeners.indexOf(fn);
+        currentListeners.splice(index, 1);
+        isSubscribed = false;
+      };
+    },
+  };
+};
 
-const createStore=(reducer, preLoadedState)=>{
-    let currentState = preLoadedState
-    let currentListeners = []
-
-    return {
-        getState(){
-            return currentState
-        },
-        dispatch(action){
-            currentState = reducer(currentState, action)
-            currentListeners.forEach(fn=>fn(currentState))
-            return action
-        },
-        subscribe(fn){
-            let isSubscribed = true
-            currentListeners.push(fn)
-            return ()=>{
-                if(!isSubscribed) return 
-                const index = currentListeners.indexOf(fn)
-                currentListeners.splice(index,1)
-                isSubscribed = false
-            }
-        }
-    }
-}
-
-export {createStore}
+export { createStore };
